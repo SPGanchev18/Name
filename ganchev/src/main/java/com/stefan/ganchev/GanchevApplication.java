ca18ed29e0374724cd74b5f14beb2e1912565ad4
Stefan Ganchev <56921662+SPGanchev18@users.noreply.github.com>
2023-01-18 15:47:23 +0200

    Add files via upload

---
+++ ganchev/src/main/java/com/stefan/ganchev/GanchevApplication.java
@@ -0,0 +1,13 @@
+package com.stefan.ganchev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanchevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanchevApplication.class, args);
+	}
+
+}
---
+++ ganchev/src/test/java/com/stefan/ganchev/GanchevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefan.ganchev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GanchevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
